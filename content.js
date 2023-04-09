f0a17624425e87788c31f8cbe4c2e1a9ba026084
edible <68762070+edibleporg@users.noreply.github.com>
2023-04-09 05:37:51 -0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,2 @@
+document.getElementById('energy').textContent = 100;
+// document.getElementById('energy').textContent = document.getElementById('energy').textContent-1;
---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('carbon').textContent = selectedCompany.carbon;
+
+// const setDOMInfo = info => {  
+//     let selectedCompany = companyData.filter(company => (
+//       company.companyName === info.manufacturer
+//     ))
+//     selectedCompany = selectedCompany[0];
+//     let score = 100 - (selectedCompany.mainScore * 2);
